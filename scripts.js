a92467f379c88b455e33003eb13134f8fb13dcdf
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-14 12:46:08 -0500

    finish wensday script

--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
 const form = document.querySelector('form');
 
-const ul = document.createElement('ul'); /* adds a empty unordered list */
+document.body.appendChild(document.createElement('ul'));
 
-document.body.appendChild(ul);
+const ul = document.querySelector('ul'); /* adds a empty unordered list */
 
 form.addEventListener('submit', event => {
   event.preventDefault(); /* prevents page from refreshing on submit which is the browsers default behavior */
-  // TODO: Take the tex from the input
+  // TODO: Take the tex from the input  [DONE]
   // TODO: Wrap that in an '<li></li>'
   // TODO: Append that to the '<ul></ul>'
-  console.log(
-    event.target.elements[0].value,
-  ); /* will pull out text value from the form, thats why we use elements[0] */
+  const newToDo = `<li>${event.target.elements[0].value}</li>`;
+  /* will pull out text value from the form, thats why we use elements[0] */
+
+  ul.innerHTML = newToDo;
+  // innerHTML - just assign a string that has HTML inside of it
 });
