20902dab6e224828ef141d72e76e359669350055
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-13 22:36:36 -0500

    Create working script for 'submit' to console.log

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+const form = document.querySelector('form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const newTask = Object.fromEntries(new FormData(event.target));
+  console.log(newTask);
+});
