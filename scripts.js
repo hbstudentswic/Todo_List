ce860b3d6558840b7760e3f726c4c6d23497e1c7
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-14 12:12:16 -0500

    add array and pushing to the array in script.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 const form = document.querySelector('form');
+const task = [];
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const newTask = Object.fromEntries(new FormData(event.target));
-  console.log(newTask);
+  console.log(newTask); /* To be removed */
+  task.push(newTask);
+  const div = document.createElement('div');
 });
