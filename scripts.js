70a19347bd78e706b00210c3fcb5a02a59be0d02
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-14 12:26:27 -0500

    start redo script for in-class

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 const form = document.querySelector('form');
-const task = [];
+
+const ul = document.createElement('ul');
+
+document.body.appendChild(ul);
 
 form.addEventListener('submit', event => {
-  event.preventDefault();
-  const newTask = Object.fromEntries(new FormData(event.target));
-  console.log(newTask); /* To be removed */
-  task.push(newTask);
-  const div = document.createElement('div');
+  console.log(event);
 });
