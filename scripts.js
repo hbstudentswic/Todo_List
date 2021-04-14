a3a858407faa23e11b08fa163b7fbe1343a0b661
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-14 12:37:28 -0500

    log input value on submition

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 const form = document.querySelector('form');
 
-const ul = document.createElement('ul');
+const ul = document.createElement('ul'); /* adds a empty unordered list */
 
 document.body.appendChild(ul);
 
 form.addEventListener('submit', event => {
-  console.log(event);
+  event.preventDefault(); /* prevents page from refreshing on submit which is the browsers default behavior */
+  // TODO: Take the tex from the input
+  // TODO: Wrap that in an '<li></li>'
+  // TODO: Append that to the '<ul></ul>'
+  console.log(
+    event.target.elements[0].value,
+  ); /* will pull out text value from the form, thats why we use elements[0] */
 });
